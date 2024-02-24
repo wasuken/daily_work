01662e20acb623ff895d5855b23faeed7fd875db
wasuken <wevorence@gmail.com>
2024-02-24 19:10:39 +0900

    update

---
+++ 2024/02/03/06_saturday_data_visualization/main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+# データの読み込み
+# データの探索とクリーニング
+# データの分析
+# データの視覚化
+
+'''
+1. 仮想目的
+A社の
+'''
+
+# データの読み込み
+df = pd.read_csv('./data.csv')
+# 商品ごとの売れた数量順
+print(df.groupby('ProductName')['Quantity'].sum().sort_values(ascending=False))
+# チャレンジ: 季節別
